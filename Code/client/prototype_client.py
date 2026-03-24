64a90419799168fb342265f258f2e48f81023b82
huynhatlam <lam54993@gmail.com>
2026-03-24 19:12:41 +0700

    adjust some change by importing protocol.py

--- Code/client/prototype_client.py
+++ Code/client/prototype_client.py
@@ -1,9 +1,13 @@
 import socket
 import threading
 import json
-import datetime
-import struct
 import queue
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from shared import protocol
+
 SERVER_IP='0.0.0.0'
 PORT=8000
 """
@@ -12,110 +16,53 @@ PORT=8000
  
 msg_queue = queue.Queue() # dùng làm queue để tương tác gui
 
-def login(username):
-    msg = {
-        "type": "LOGIN",
-        "username": username,
-        "timestamp": datetime.datetime.now().isoformat()
-    }
-    return msg
-
-def join(username):
-    msg = {
-        "type": "JOIN",
-        "username": username,
-        "timestamp": datetime.datetime.now().isoformat()
-    }
-    return msg
-
-def chat(username, content):
-    msg = {
-        "type": "CHAT",
-        "sender": username,
-        "content": content,
-        "timestamp": datetime.datetime.now().isoformat()
-    }
-    return msg
-
-def private(sender, receiver, content):
-    msg = {
-        "type": "PRIVATE",
-        "sender": sender,
-        "receiver": receiver,
-        "content": content,
-        "timestamp": datetime.datetime.now().isoformat()
-    }
-    return msg
- 
-def leave(username):
-    msg = {
-        "type": "LEAVE",
-        "username": username,
-        "timestamp": datetime.datetime.now().isoformat()
-    }
-    return msg
- 
-def error_msg(error):
-    msg = {
-        "type": "ERROR",
-        "content": error,
-        "timestamp": datetime.datetime.now().isoformat()
-    }
-    return msg
-
-
-def recvall(sock, n):
-    data = b''
-    while len(data) < n:
-        packet = sock.recv(n - len(data))
-        if not packet:
-            return None
-        data += packet
-    return data
-
-def encode_message(msg):
-    return json.dumps(msg)
-
-def decode_message(data):
-    return json.loads(data)
 
 def recieve_loop(client,username):
-    while True:
+   while True:
         try:
-            raw_length = recvall(client,4)
-            if not raw_length:
-                print("[DISCONNECT] Server closed connection")
-                disconnect(client,username)
+            msg = protocol.recv_message(client)
+            if msg is None:
+                print("không nhận được dữ liệu hoặc lỗi")
+                disconnect(client, username)
                 break
-            try:
-                length = struct.unpack("!I", raw_length)[0]
-                data = recvall(client, length)
-                msg = decode_message(data.decode("utf-8")) 
-                if msg["type"] == "LOGIN_SUCCESS":
-                    send_message(client,join(username)) 
-                if msg["type"] == "LOGIN_FAIL":
-                    msg_queue.put(error_msg(msg["type"])) # khi nhận message thì gửi vào queue gui sẽ lấy message để cập nhật gui
-                    disconnect(client,username)
-                    break
-                if msg["type"] == "NOTICE":
-                   msg_queue.put(msg)
-                if msg["type"] == "CHAT":
-                   msg_queue.put(msg)
-                if msg["type"] == "PRIVATE":
-                    if(msg['receiver'] == username):
-                        msg_queue.put(msg)
-                    else:
-                        pass
 
-            except json.JSONDecodeError:
-                msg_queue.put(error_msg(f"[SERVER RAW] lỗi biên dịch json show json raw: {data.decode('utf-8')}"))
+            if msg["type"] == "LOGIN_SUCCESS":
+                protocol.send_message(client, protocol.join(username))
+
+            elif msg["type"] == "LOGIN_FAIL":
+                msg_queue.put(protocol.error_msg(msg["type"]))
+                disconnect(client, username)
+                break
+
+            elif msg["type"] == "NOTICE":
+                msg_queue.put(msg)
+
+            elif msg["type"] == "CHAT":
+                print(msg["content"])
+                msg_queue.put(msg)
+
+            elif msg["type"] == "PRIVATE":
+                if msg['receiver'] == username:
+                   msg_queue.put(msg)
+        
+        except json.JSONDecodeError as e:
+            raw_data = getattr(e, "doc", None)
+            msg_queue.put(protocol.error_msg(
+                f"[SERVER RAW] lỗi biên dịch JSON, dữ liệu: {raw_data}"
+            ))
+            # không break, chỉ bỏ qua message lỗi và tiếp tục vòng lặp
+            continue
+
         except Exception as e:
-            msg_queue.put(error_msg(f"[ERROR] Connection lost: {e}"))
-            break
+            msg_queue.put(protocol.error_msg(f"Lỗi không xác định: {e}"))
+            disconnect(client, username)   # đóng socket luôn
+            return  # kết thúc thread
+
+
 
 def disconnect(client,username):
     try:
-        send_message(client,leave(username))
+        protocol.send_message(client, protocol.leave(username))
         client.shutdown(socket.SHUT_RDWR) 
     except OSError as e:
         print(f"Error: {e}")
@@ -123,17 +70,6 @@ def disconnect(client,username):
     finally:
         client.close()
     
-
-def send_message(client,dict): # done
-    try:
-        data = encode_message(dict).encode("utf-8")
-        length = struct.pack("!I", len(data))
-        client.sendall(length + data)
-    except OSError as e:
-        print(f"[ERROR] Failed to send message: {e}")
-    except Exception as e:
-         print(f"[ERROR] Unexpected error: {e}")
-
 
 def connect_to_server(host,port): # done
     try:
@@ -148,7 +84,8 @@ def connect_to_server(host,port): # done
 
 client = connect_to_server(SERVER_IP,PORT)
 username = input("You name: ")
-send_message(client,login(username))
+protocol.send_message(client,protocol.login(username))
+
 threading.Thread(target=recieve_loop,args = (client,username), daemon=True).start()
 
 while True:
@@ -162,11 +99,11 @@ while True:
         if len(parts) >= 3:
             receiver = parts[1]
             content = parts[2]
-            send_message(client,private(username,receiver,content))
+            protocol.send_message(client,protocol.private_msg(username,receiver,content))
             print(f"YOU -> {receiver}: {content}")
         else:
             print("Sai cú pháp, vui lòng nhập lại")
             continue
     else:    
-        send_message(client,chat(username,msg))
+        protocol.send_message(client,protocol.chat(username,msg))
         print(f"YOU: {msg}")
